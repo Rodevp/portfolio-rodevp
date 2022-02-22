b531f4dc35fd3df80c4e7fdbd28d486b0793cde6
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-21 22:28:54 -0500

    haciendo seccion de proyectos react y harcodeando layout de una card

--- web/src/projects/Projects.jsx
+++ web/src/projects/Projects.jsx
@@ -3,53 +3,85 @@ import { vanilla } from './data.js'
 
 function Projects() {
 
-  const vanillaProjects = [...vanilla] 
+    const vanillaProjects = [...vanilla]
 
-  return (
-    <section className={projectsClasses.projects}>
-        <h2 className={projectsClasses.projects__title}>
-            Experiencia
-        </h2>
-        <section className={projectsClasses.projects__wrapper}>
-            <div className={projectsClasses.projects__vanilla}>
-                <h2 className={projectsClasses.projects__vanilla_title}>
-                    <p>Html</p>
-                    <p>Css</p>
-                    <p>Javascript</p>
-                </h2>
-                <section className={projectsClasses.projects__js}>
-                    {
-                        vanillaProjects.map(project => (
-                            <div key={project.title} className={projectsClasses.vanilla_card}>
-                                <div  className={projectsClasses.vanilla_img}>
-                                    <img src={project.img} alt='project image' />
+    return (
+        <section className={projectsClasses.projects}>
+            <h2 className={projectsClasses.projects__title}>
+                Experiencia
+            </h2>
+            <section className={projectsClasses.projects__wrapper}>
+                <div className={projectsClasses.projects__vanilla}>
+                    <h2 className={projectsClasses.projects__vanilla_title}>
+                        <p>Html</p>
+                        <p>Css</p>
+                        <p>Javascript</p>
+                    </h2>
+                    <section className={projectsClasses.projects__js}>
+                        {
+                            vanillaProjects.map(project => (
+                                <div key={project.title} className={projectsClasses.vanilla_card}>
+                                    <div className={projectsClasses.vanilla_img}>
+                                        <img src={project.img} alt='project image' />
+                                    </div>
+                                    <h2 className={projectsClasses.vanilla_title_card}>
+                                        {project.title}
+                                    </h2>
+                                    <section className={projectsClasses.vanilla_desc}>
+                                        {project.desc}
+                                    </section>
+                                    <section className={projectsClasses.vanilla_links_content}>
+                                        <a
+                                            href={project.gitUrl}
+                                            className={projectsClasses.vanilla_btn}
+                                        >
+                                            Ver Código
+                                        </a>
+                                        <a
+                                            href={project.demoUrl}
+                                            className={projectsClasses.vanilla_btn}
+                                        >
+                                            Ver Demo
+                                        </a>
+                                    </section>
                                 </div>
-                                <h2 className={projectsClasses.vanilla_title_card}>
-                                    {project.title}
-                                </h2>
-                                <section  className={projectsClasses.vanilla_desc}>
-                                    {project.desc}
-                                </section>
-                                <section  className={projectsClasses.vanilla_links_content}>
-                                    <a href={project.gitUrl} className={projectsClasses.vanilla_btn}>Ver Código</a>
-                                    <a href={project.demoUrl} className={projectsClasses.vanilla_btn}>Ver Demo</a>
-                                </section>
+                            ))
+                        }
+                    </section>
+                </div>
+                <div className={projectsClasses.projects__react}>
+                    <h2 className={projectsClasses.projects__react_title} >
+                        React Js ⚛️
+                    </h2>
+                    <section className={projectsClasses.projects__react}>
+                        <div className={projectsClasses.react_card}>
+                            <div className={projectsClasses.react__card_img}>
+                                <img src={''} alt='project image' />
                             </div>
-                        ))
-                    }
-                </section>
-            </div>
-            <div>
-                <h2>
-                   React Js ⚛️
-                </h2>
-                <section>
-                    
-                </section>
-            </div>
+                            <h2 className={projectsClasses.react__title_card}>
+                            </h2>
+                            <section className={projectsClasses.react__card_desc}>
+                            </section>
+                            <section className={projectsClasses.card__react_links_content}>
+                                <a
+                                    href={''}
+                                    className={projectsClasses.card__react_btn}
+                                >
+                                    Ver Código
+                                </a>
+                                <a
+                                    href={''}
+                                    className={projectsClasses.card__react_btn}
+                                >
+                                    Ver Demo
+                                </a>
+                            </section>
+                        </div>
+                    </section>
+                </div>
+            </section>
         </section>
-    </section>
-  )
+    )
 }
 
 export default Projects
