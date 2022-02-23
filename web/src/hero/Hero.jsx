f34dae42ee471b9c3f0d6ffcdcedc15f424b1534
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-23 17:36:33 -0500

    colocando imagen mia .__.

--- web/src/hero/Hero.jsx
+++ web/src/hero/Hero.jsx
@@ -1,12 +1,13 @@
 import heroClasses from './hero.module.css'
 import logoJs from '../images/js.png'
+import imgMe from '../images/me.jpg'
 
 function Hero() {
     return (
         <section className={heroClasses.hero}  id='main'>
             <div className={heroClasses.hero__me}>
                 <section className={heroClasses.img__content}>
-                    <img src="" alt="" />
+                    <img src={imgMe} alt='image person' />
                 </section>
             </div>
             <section className={heroClasses.hero__description}>
