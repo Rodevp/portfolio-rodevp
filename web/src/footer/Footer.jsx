958298622dae840e41d46d9a54c8cb445a39ef60
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-24 12:24:41 -0500

    eliminando imagenes inecesarias, max width a header y main, y agregando redes sociales al inicio

--- web/src/footer/Footer.jsx
+++ web/src/footer/Footer.jsx
@@ -1,34 +1,10 @@
 import classesFooter from './footer.module.css'
-import logoLinkedin from '../images/linkedin.png'
-import logoTwitter from '../images/twitter.png'
-import logoGit from '../images/git.png'
 
 function Footer() {
 
     return (
         <footer className={classesFooter.footer} id='footer'>
             <div className={classesFooter.network}>
-                <a 
-                    href='https://www.linkedin.com/in/rodrigo-rodriguez-046712181'
-                    target='_blank'
-                    className={classesFooter.network_item}
-                >
-                    <img src={logoLinkedin} alt='logo linkedin' />
-                </a>
-                <a 
-                    href='https://twitter.com/rodevp'
-                    target='_blank'
-                    className={classesFooter.network_item}
-                >
-                    <img src={logoTwitter} alt='logo twitter' />
-                </a>
-                <a
-                    href='https://github.com/Rodevp'
-                    target='_blank'
-                    className={classesFooter.network_item}
-                >
-                    <img src={logoGit} alt='logo github' />
-                </a>
             </div>
             <div className={classesFooter.copy_content}>
                 <p>
--- web/src/hero/Hero.jsx
+++ web/src/hero/Hero.jsx
@@ -1,8 +1,15 @@
 import heroClasses from './hero.module.css'
 import logoJs from '../images/js.png'
 import imgMe from '../images/me.jpg'
+import imgLinkedin from '../images/linkedin.svg'
+import imgGit from '../images/github.svg'
+import imgTwitter from '../images/twitter.svg'
+
 
 function Hero() {
+
+    console.log(imgGit, imgLinkedin, imgLinkedin)
+
     return (
         <section className={heroClasses.hero}  id='main'>
             <div className={heroClasses.hero__me}>
@@ -21,6 +28,35 @@ function Hero() {
                             <img src={logoJs} alt='logo javascript' />
                         </span>
                     </p>
+                    <p className={heroClasses.hero__description_sentence}>
+                        🍦 Captador profesional de los helados de vainilla. 🍨
+                    </p>
+                    <div className={heroClasses.hero__network}>
+                        <a 
+                            href='https://www.linkedin.com/in/rodrigo-rodriguez-046712181/'
+                            target='_blank'
+                            className={heroClasses.hero__network_item}
+                            >
+                            <p>Linkedin</p>
+                            <img src={imgLinkedin} alt='logo linkedin' />
+                        </a>
+                        <a 
+                            href='https://github.com/Rodevp'
+                            target='_blank'
+                            className={heroClasses.hero__network_item}
+                            >
+                            <p>Github</p>
+                            <img src={imgGit} alt='logo git' />
+                        </a>
+                        <a 
+                            href='https://twitter.com/rodevp'
+                            target='_blank'
+                            className={heroClasses.hero__network_item}
+                            >
+                            <p>Twitter</p>
+                            <img src={imgTwitter} alt='logo twitter' />
+                        </a>
+                    </div>
                 </h1>
             </section>
         </section>
