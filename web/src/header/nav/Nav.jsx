e1be1d8a6a4214607a9ca1610e81a4bfd91ff85d
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-27 11:27:20 -0500

    agregando funcion de cerrar menu al menu hamburguesa

--- web/src/header/nav/Nav.jsx
+++ web/src/header/nav/Nav.jsx
@@ -1,32 +1,39 @@
 import { useEffect } from 'react'
 import navClasses from '../css/nav.module.css'
-import menuHamburgerImage from '../../images/menu.png'
 
 function Nav() {
+
+    let menuItems;
+    let hamburguer;
 
     useEffect(() => {
 
         const menuHamburger = document.getElementById('hamburguer')
-    
         const menu = document.getElementById('menu')
+        menuItems = menu
+        hamburguer = menuHamburger
     
         menuHamburger.addEventListener('click', e => {
             menu.classList.toggle(navClasses.open__menu)
-            console.log(menu)
-            console.log('click')
-    
+            menuHamburger.classList.toggle(navClasses.hamburguer__menu__close)
         })
     
     }, [])
 
+    const closeMenu = e => {
+        menuItems.classList.toggle(navClasses.open__menu)
+        hamburguer.classList.toggle(navClasses.hamburguer__menu__close)
+    }
+
   return (
     <nav className={navClasses.nav}>
-        <img src={menuHamburgerImage} alt='image of menu' id='hamburguer' width='30px' />
+        <div  id='hamburguer' className={navClasses.hamburguer__menu} >
+        </div>
         <ul className={navClasses.menu} id='menu'>
-            <li className={navClasses.menu__item}><a href='#main'>Inicio</a></li>
-            <li className={navClasses.menu__item}><a href='#about'>Sobre mi </a></li>
-            <li className={navClasses.menu__item}><a href='#projects'>Proyectos </a></li>
-            <li className={navClasses.menu__item}><a href='#footer'>Contacto</a></li>
+            <li className={navClasses.menu__item}><a href='#main' onClick={closeMenu}>Inicio</a></li>
+            <li className={navClasses.menu__item}><a href='#about' onClick={closeMenu}>Sobre mi </a></li>
+            <li className={navClasses.menu__item}><a href='#projects' onClick={closeMenu}>Proyectos </a></li>
+            <li className={navClasses.menu__item}><a href='#footer' onClick={closeMenu} >Contacto</a></li>
         </ul>
     </nav>
   )
