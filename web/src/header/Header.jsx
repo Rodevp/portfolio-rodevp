af7fadba18f76bf7a72d16b063a719cfa4c82429
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-27 11:09:36 -0500

    creando animacion de menu y media querie

--- web/src/header/Header.jsx
+++ web/src/header/Header.jsx
@@ -3,12 +3,13 @@ import headerClasses from './css/header.module.css'
 import logo from '../images/logo_rodevp.png'
 
 function Header() {
+
   return (
     <header className={headerClasses.header}>
         <div className={headerClasses.content__img}>
             <img src={logo} alt="image of logo" />
         </div>
-        <div className={headerClasses.content__nav}>
+        <div className={headerClasses.content__nav} id='content_nav'>
             <Nav />
         </div>
     </header>
--- web/src/header/nav/Nav.jsx
+++ web/src/header/nav/Nav.jsx
@@ -5,18 +5,23 @@ import menuHamburgerImage from '../../images/menu.png'
 function Nav() {
 
     useEffect(() => {
-        const menuHamburger = document.getElementById('hamburguer')
-        const menu = document.getElementById('menu')
 
+        const menuHamburger = document.getElementById('hamburguer')
+    
+        const menu = document.getElementById('menu')
+    
         menuHamburger.addEventListener('click', e => {
             menu.classList.toggle(navClasses.open__menu)
+            console.log(menu)
+            console.log('click')
+    
         })
-
+    
     }, [])
 
   return (
     <nav className={navClasses.nav}>
-        <img src={menuHamburgerImage} alt='image of menu' id='hamburguer' />
+        <img src={menuHamburgerImage} alt='image of menu' id='hamburguer' width='30px' />
         <ul className={navClasses.menu} id='menu'>
             <li className={navClasses.menu__item}><a href='#main'>Inicio</a></li>
             <li className={navClasses.menu__item}><a href='#about'>Sobre mi </a></li>
