a875eac8d161085941d71d686db2fc875a6e8329
Rodevp <rodriguezrodrigor12@gmail.com>
2022-03-17 11:35:22 -0500

    maquetando header y creando sus componentes

---
+++ portfolio/src/header/Header.vue
@@ -0,0 +1,78 @@
+<template>
+  <header class="header">
+    <div class="content__img">
+      <img src="#" alt="image of logo" />
+    </div>
+    <div class="content__nav">
+      <Nav />
+    </div>
+  </header>
+</template>
+
+<script>
+import Nav from "./Nav.vue";
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  font-family: var(--font);
+  background-color: var(--background);
+  display: flex;
+  justify-content: center;
+  position: sticky;
+  top: 0;
+  z-index: 3;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  padding-top: 2rem;
+  padding-bottom: 1rem;
+  max-width: 920px;
+  margin: auto;
+}
+
+.content__img {
+  width: 45%;
+}
+
+.content__img img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 1px solid var(--color-purple-border-card);
+}
+
+.content__nav {
+  width: 45%;
+}
+
+@media screen and (min-width: 620px) {
+  .content__img {
+    width: 30%;
+  }
+
+  .content__nav {
+    display: flex;
+    justify-content: flex-end;
+    width: 70%;
+  }
+}
+
+@media screen and (min-width: 770px) {
+  .header {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+}
+
+@media screen and (min-width: 920px) {
+  .content__nav {
+    justify-content: flex-end;
+  }
+}
+</style>
---
+++ portfolio/src/header/Nav.vue
@@ -0,0 +1,130 @@
+<template>
+  <nav className="nav">
+        <div  id='hamburguer' className="hamburguer__menu" >
+        </div>
+        <ul className="menu" id='menu'>
+            <li className="menu__item"><a href='#main'>Inicio</a></li>
+            <li className="menu__item"><a href='#about'>Sobre mi </a></li>
+            <li className="menu__item"><a href='#projects'>Proyectos </a></li>
+            <li className="menu__item"><a href='#footer' >Contacto</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.nav {
+    display: flex;
+    flex-direction: column;
+    padding-top: 1em;
+}
+
+.hamburguer__menu {
+    width: 30px;
+    height: 30px;
+    align-self: flex-end;
+    position: relative;
+    cursor: pointer;
+    background-image: url('../../images/menu.png');
+}
+
+.hamburguer__menu__close {
+    width: 32px;
+    height: 32px;
+    background-repeat: no-repeat;
+    object-fit: contain;
+    background-image: url('../../images/marca-x.png')
+}
+
+.menu {
+    width: 42%;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    opacity: 0;
+    top: 3rem;
+    left: 55%;
+    align-items: center;
+    background-color:  var(--color-purple-btn);
+    overflow: hidden;
+    margin-top: 2rem;
+}
+
+.open__menu {
+    opacity: 1;
+    transition: opacity 0.25s linear;
+}
+
+.menu__item {
+    width: 90%;
+    padding: 1em 0;
+    margin-left: 1rem;
+    font-family: var(--font);
+    font-size: 1rem;
+    list-style: none;
+    color: var(--background);
+    cursor: pointer;
+    transition: color 1ms;
+}
+
+.menu__item a {
+    text-decoration: none;
+    color: inherit;
+    font-family: inherit;
+}
+
+.menu__item:hover {
+    color: var(--color-border-about-me-title);
+}
+
+@media screen and (min-width: 770px) {
+
+    .nav {
+        display: flex;
+        align-items: center;
+        padding: 0;
+        width: 80%;
+    }
+    
+    .hamburguer__menu {
+        display: none;
+    }
+    
+    .menu {
+        left: 0;
+        opacity: 1;
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        transform: none;
+        width: 100%;
+        height: 100%;
+        margin-top: 0;
+        top: 0;
+        flex-direction: row;
+        position: relative; 
+        background: none;
+    }
+    
+    .menu__item {
+        margin: 0;
+        padding: 1em 0;
+        font-weight: 400;
+        text-align: center;
+        font-family: var(--font-title);
+        color: var(--color-purple-btn);
+    }
+
+}
+
+@media screen and (min-width: 920px) {
+    .menu__item {
+        font-size: 1.1rem;
+    }
+}
+</style>
